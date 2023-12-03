39893ae6321623a7709e8d0ce8f82fa151055ca0
Abdullah Ibne Atiq <hello@abdu.io>
2023-12-03 15:14:53 +0000

    Create Page component

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Grid } from './Grid'
 export { default as SarfSagheer } from './SarfSagheer'
 export { default as Conjugations } from './Conjugations'
+export { default as Page } from './Page'
