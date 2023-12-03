d00c50ce56f1bb525bb0fc8b6b7b33458b97438b
Abdullah Ibne Atiq <hello@abdu.io>
2023-12-03 15:12:57 +0000

    Create Navbar component

---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,109 @@
+import { useLocation, useRoute } from 'preact-iso'
+import './Navbar.scss'
+import { useCallback, useMemo } from 'preact/hooks'
+import verbTypes from '../../../data'
+import { RomanNumeral, VerbChapter } from '../../../data/types'
+import { replaceRoots } from '../../Helpers'
+
+const Navbar = () => {
+  const { verbChapter, verbForm, verbType } = useRoute().params
+
+  const location = useLocation()
+
+  const goToHome = useCallback(() => location.route('/'), [])
+
+  const chapters = useMemo(() => verbTypes[verbType], [verbType])
+
+  const mujarradOptions = useMemo(() => {
+    if (!chapters) return []
+    return Object.keys(chapters['I']).map((chapter) => ({
+      key: chapter,
+      value: `I/${chapter}`,
+    }))
+  }, [chapters])
+
+  const mazidFihOptions = useMemo(() => {
+    if (!chapters) return []
+
+    const options: Array<{ key: string; value: string }> = []
+
+    for (const form of Object.keys(chapters)) {
+      const $form = form as RomanNumeral
+      if ($form === 'I') continue
+      const baseForm = chapters[$form as RomanNumeral] as VerbChapter
+      if (!baseForm) continue
+      const chapter = replaceRoots(baseForm, {
+        ف: baseForm.archetype.root_letters[0],
+        ع: baseForm.archetype.root_letters[1],
+        ل: baseForm.archetype.root_letters[2],
+      })
+      if (chapter)
+        options.push({
+          key: `${form} ${chapter.archetype.ماضي.معروف}`,
+          value: form,
+        })
+    }
+
+    return options
+  }, [chapters])
+
+  const goToVerbType = useCallback((event: { target: EventTarget | null }) => {
+    if (!event.target) return
+    if ('value' in event.target) location.route(`/${event.target.value}`)
+  }, [])
+
+  const goToChapter = useCallback((event: { target: EventTarget | null }) => {
+    if (!event.target) return
+    if ('value' in event.target)
+      location.route(`/${verbType}/${event.target.value}`)
+  }, [])
+
+  return (
+    <div class="navbarContainer">
+      <button onClick={goToHome}>Home</button>
+
+      {verbType && (
+        <>
+          <span>&rsaquo;</span>
+
+          <select value={verbType} onChange={goToVerbType}>
+            {Object.keys(verbTypes).map((verbType) => (
+              <option key={verbType} value={verbType}>
+                {verbType}
+              </option>
+            ))}
+          </select>
+        </>
+      )}
+
+      {verbForm && (
+        <>
+          <span>&rsaquo;</span>
+
+          <select
+            value={verbForm === 'I' ? `I/${verbChapter}` : verbChapter}
+            onChange={goToChapter}
+          >
+            <optgroup label="مجرّد">
+              {mujarradOptions.map((chapter) => (
+                <option key={chapter.key} value={chapter.value}>
+                  {chapter.key}
+                </option>
+              ))}
+            </optgroup>
+
+            <optgroup label="مزيد فِه">
+              {mazidFihOptions.map((chapter) => (
+                <option key={chapter.key} value={chapter.value}>
+                  {chapter.key}
+                </option>
+              ))}
+            </optgroup>
+          </select>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Navbar
