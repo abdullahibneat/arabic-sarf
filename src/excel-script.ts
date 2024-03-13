51ce33152501ca26b98686c6f559a3f5319c4934
Abdullah Ibne Atiq <hello@abdu.io>
2024-03-13 22:49:10 +0000

    Script to generate cells for Excel spreadsheets

---
+++ src/excel-script.ts
@@ -0,0 +1,70 @@
+import verbTypes from '../data'
+import { VerbChapter, VerbTasreef } from '../data/types'
+import { replaceRoots } from './Helpers'
+
+const cells: string[] = []
+
+const processVerbChapter = (
+  cells: string[],
+  chapterName: string,
+  verbChapter: VerbChapter | null,
+) => {
+  if (!verbChapter) {
+    cells.push(`${chapterName} - Madi`)
+    cells.push('')
+    cells.push(`${chapterName} - Mudari`)
+    cells.push('')
+    return
+  }
+
+  const [ف, ع, ل] = verbChapter.archetype.root_letters
+
+  const verbChapterWithRoots = replaceRoots(verbChapter, { ف, ع, ل })
+
+  cells.push(`${chapterName} - Madi`)
+  prepareTasreefs(cells, verbChapterWithRoots.conjugations.ماضي.معروف)
+  cells.push(`${chapterName} - Mudari`)
+  prepareTasreefs(cells, verbChapterWithRoots.conjugations.مضارع.معروف)
+}
+
+const prepareTasreefs = (cells: string[], tasreef: VerbTasreef | null) => {
+  if (!tasreef) {
+    cells.push('')
+    return
+  }
+
+  cells.push(tasreef['3rd'].masculine['هُوَ'])
+  cells.push(tasreef['3rd'].masculine['هُمَا'])
+  cells.push(tasreef['3rd'].masculine['هُمْ'])
+  cells.push(tasreef['3rd'].feminine['هِيَ'])
+  cells.push(tasreef['3rd'].feminine['هُمَا'])
+  cells.push(tasreef['3rd'].feminine['هُنَّ'])
+
+  cells.push(tasreef['2nd'].masculine['أَنْتَ'])
+  cells.push(tasreef['2nd'].masculine['أَنْتُمَا'])
+  cells.push(tasreef['2nd'].masculine['أَنْتُمْ'])
+  cells.push(tasreef['2nd'].feminine['أَنْتِ'])
+  cells.push(tasreef['2nd'].feminine['أَنْتُمَا'])
+  cells.push(tasreef['2nd'].feminine['أَنْتُنَّ'])
+
+  cells.push(tasreef['1st'].أَنَا)
+  cells.push(tasreef['1st'].نَحْنُ)
+  cells.push('Fluency')
+  cells.push('')
+}
+
+for (const [verbTypeName, verbType] of Object.entries(verbTypes)) {
+  cells.push(verbTypeName) // صحيح, أجوف, ناقص
+  cells.push('')
+
+  for (const [chapterName, chapter] of Object.entries(verbType)) {
+    if (!chapter || (chapter && 'archetype' in chapter)) {
+      processVerbChapter(cells, chapterName, chapter as VerbChapter | null)
+    } else {
+      // FORM 1
+      for (const [form1ChapterName, form1Chapter] of Object.entries(chapter)) {
+        processVerbChapter(cells, `I - ${form1ChapterName}`, form1Chapter)
+      }
+    }
+  }
+}
