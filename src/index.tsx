535d245f65f3387eddf9e618969a1d00170b450e
Abdullah Ibne Atiq <hello@abdu.io>
2024-03-26 00:35:54 +0000

    Configure responsive app layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import { render } from 'preact'
-import { LocationProvider, Router, Route } from 'preact-iso'
+import './styles/App.scss'
+
+import { LocationProvider, Route, Router } from 'preact-iso'
+
 import Home from './pages/Home'
 import NotFound from './pages/_404'
+import Sidebar from './components/Sidebar'
+import { render } from 'preact'
 
 const App = () => {
   return (
     <LocationProvider>
-      <main>
-        <Router>
-          <Route path="/" component={Home} />
-          <Route default component={NotFound} />
-        </Router>
-      </main>
+      <div class="app-container">
+        <main>
+          <Router>
+            <Route path="/" component={Home} />
+            <Route default component={NotFound} />
+          </Router>
+        </main>
+
+        <Sidebar />
+      </div>
     </LocationProvider>
   )
 }
