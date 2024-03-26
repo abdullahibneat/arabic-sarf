bd317ba5318ef8dcfeda1625ba844b4b616cf2b4
Abdullah Ibne Atiq <hello@abdu.io>
2024-03-26 00:34:48 +0000

    Show madi tasreefs in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import Flex from '../components/Flex'
+import Tasreef from '../components/Tasreef'
+import isVerbChapter from '../helpers/isVerbChapter'
+import { useMemo } from 'preact/hooks'
+import verbTypes from '../../data'
+
 const Home = () => {
-  return <div>Hello world!</div>
+  const { صحيح } = verbTypes
+
+  const verbChapters = useMemo(
+    () =>
+      Object.values(صحيح).flatMap((value) => {
+        if (!value) return []
+        if (isVerbChapter(value)) return value
+        return Object.values(value)
+      }),
+    [صحيح],
+  )
+
+  return (
+    <Flex gap={32} padding={32} direction="rtl" overflowX="auto">
+      {verbChapters.map((verbChapter) => (
+        <Tasreef key={verbChapter.باب} verbChapter={verbChapter} />
+      ))}
+    </Flex>
+  )
 }
 
 export default Home
