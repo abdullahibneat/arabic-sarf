97beaa5a78eb61e3b3d16aa73c9dc5d4531a848d
Abdullah Ibne Atiq <hello@abdu.io>
2024-03-26 00:28:05 +0000

    Set up AppState class

---
+++ src/AppState.tsx
@@ -0,0 +1,76 @@
+const LOCAL_STORAGE_KEY = 'state'
+
+export type AppStateType = {
+  tasreefGroupMode: 'list' | 'by-person' | 'by-gender'
+}
+
+export type SetItemListener = (state: AppStateType) => void
+
+const defaultState: AppStateType = {
+  tasreefGroupMode: 'list',
+}
+
+class State {
+  private state = defaultState
+  private setItemListeners: SetItemListener[] = []
+
+  constructor() {
+    this.loadFromLocalStorage()
+    this.setItemListeners.push(this.saveToLocalStorage)
+  }
+
+  get = () => {
+    return this.state
+  }
+
+  getItem = <T extends keyof AppStateType>(key: T): AppStateType[T] => {
+    return this.state[key]
+  }
+
+  setItem = <T extends keyof AppStateType>(key: T, value: AppStateType[T]) => {
+    this.state[key] = value
+    const state = this.state
+    setTimeout(() => {
+      for (const listener of this.setItemListeners) {
+        listener({ ...state })
+      }
+    }, 0)
+  }
+
+  registerSetItemListener = (listener: SetItemListener) => {
+    this.setItemListeners.push(listener)
+
+    return {
+      remove: () => {
+        this.setItemListeners = this.setItemListeners.filter(
+          ($listener) => $listener !== listener,
+        )
+      },
+    }
+  }
+
+  private loadFromLocalStorage = () => {
+    try {
+      const stateString = localStorage.getItem(LOCAL_STORAGE_KEY)
+
+      if (!stateString) return
+
+      const localStorageState = JSON.parse(stateString)
+
+      if (
+        'tasreefGroupMode' in localStorageState &&
+        typeof localStorageState.tasreefGroupMode === 'string'
+      ) {
+        this.state.tasreefGroupMode = localStorageState.tasreefGroupMode
+      }
+    } catch (_) {
+      // ignore errors
+    }
+  }
+
+  private saveToLocalStorage = () => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state))
+  }
+}
+
+export default new State()
---
+++ src/hooks/useAppState.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'preact/hooks'
+
+import AppState from '../AppState'
+
+const useAppState = () => {
+  const [state, setState] = useState(AppState.get())
+
+  useEffect(() => {
+    const listener = AppState.registerSetItemListener((newState) => {
+      setState(newState)
+    })
+
+    return () => listener.remove()
+  }, [])
+
+  return state
+}
+
+export default useAppState
