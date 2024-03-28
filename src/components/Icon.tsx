51e9e4e440151176b7083a82260d85d3cc0b0fd4
Abdullah Ibne Atiq <hello@abdu.io>
2024-03-28 18:17:23 +0000

    Add hover color to icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,5 @@
+import '../styles/Icon.scss'
+
 import DefaultIcons from '../icons/default'
 import MicroIcons from '../icons/micro'
 import { useMemo } from 'preact/hooks'
@@ -5,20 +7,32 @@ import { useMemo } from 'preact/hooks'
 export type IconProps = {
   size?: 'default' | 'micro'
   color?: 'white' | 'text' | 'text' | 'text-secondary' | 'hover' | 'border'
+  hoverColor?: 'white' | 'text' | 'text' | 'text-secondary' | 'hover' | 'border'
   name: keyof typeof DefaultIcons
 } & (
   | { size?: 'default' | never; name: keyof typeof DefaultIcons }
   | { size: 'micro'; name: keyof typeof MicroIcons }
 )
 
-const Icon = ({ size = 'default', name, color = 'text' }: IconProps) => {
+const Icon = ({
+  size = 'default',
+  name,
+  color = 'text',
+  hoverColor = 'text',
+}: IconProps) => {
   const IconComponent = useMemo(() => {
     if (size === 'micro') return MicroIcons[name]
     return DefaultIcons[name]
   }, [size, name])
 
   return (
-    <div style={{ '--fill': `var(--${color}, text)` }}>
+    <div
+      class="icon-container"
+      style={{
+        '--base-fill': `var(--${color})`,
+        '--hover-fill': `var(--${hoverColor})`,
+      }}
+    >
       <IconComponent />
     </div>
   )
