49d931a74b2297a2b6414e5f4dc15337ae5b1a3a
Abdullah Ibne Atiq <hello@abdu.io>
2024-04-18 20:53:55 +0100

    Wire up Settings modal

--- src/modals/SettingsModal.tsx
+++ src/modals/SettingsModal.tsx
@@ -1,66 +1,180 @@
-import Segmented, { SegmentedOption } from '../components/Segmented'
+import '../styles/SettingsModal.scss'
 
+import { useCallback, useMemo } from 'preact/hooks'
+
+import AppState from '../AppState'
 import Flex from '../components/Flex'
+import Segmented from '../components/Segmented'
 import Text from '../components/Text'
-import { useState } from 'preact/hooks'
+import useAppState from '../hooks/useAppState'
+import verbTypes from '../../data'
 
 const SettingsModal = () => {
-  const options: SegmentedOption[] = [
-    { label: 'Option 1', value: 1 },
-    { label: 'Option 2', value: 2 },
-    { label: 'Option 3', value: 3 },
+  const { settings } = useAppState()
+
+  const verbTypeOptions = Object.keys(verbTypes).map((verbType) => ({
+    key: verbType,
+    name: verbType,
+    value: !settings.hiddenVerbTypes.includes(verbType),
+  }))
+
+  const mujarradChapterHeadingsOptions = useMemo(
+    () =>
+      [
+        { label: 'Arabic (ن, ض, ف)', value: 'arabic' },
+        { label: 'English (1a, 1b, 1c)', value: 'english' },
+      ] as const,
+    [],
+  )
+
+  const mazeedFihiChapterHeadingsOptions = useMemo(
+    () =>
+      [
+        { label: 'English (2,3,4)', value: 'english' },
+        { label: 'Roman (II, III, IV)', value: 'roman' },
+      ] as const,
+    [],
+  )
+
+  const booleanOptions = useMemo(
+    () =>
+      [
+        { label: 'Yes', value: true },
+        { label: 'No', value: false },
+      ] as const,
+    [],
+  )
+
+  const tasreefOptions = [
+    { key: 'showNasb', name: 'Nasb', value: settings.showNasb },
+    { key: 'showJazm', name: 'Jazm', value: settings.showJazm },
+    { key: 'showAmr', name: 'Amr', value: settings.showAmr },
+    { key: 'showMajhool', name: 'Majhool', value: settings.showMajhool },
   ]
 
-  const [option, setOption] = useState<SegmentedOption | null>(null)
+  const toggleVerbType = useCallback((verbType: string, enable: boolean) => {
+    const newSettings = AppState.getItem('settings')
+
+    const hide = !enable
+    const currentlyHidden = newSettings.hiddenVerbTypes.includes(verbType)
+
+    if (enable && currentlyHidden) {
+      newSettings.hiddenVerbTypes = newSettings.hiddenVerbTypes.filter(
+        (type) => type !== verbType,
+      )
+    } else if (hide && !currentlyHidden) {
+      newSettings.hiddenVerbTypes.push(verbType)
+    }
+
+    AppState.setItem('settings', newSettings)
+  }, [])
 
   return (
     <Flex column gap={12} width={400} padding={24} paddingTop={12}>
       <Flex column gap={4}>
         <Text>Verb types</Text>
-        <Segmented
-          value={option?.value}
-          options={options}
-          onChange={setOption}
-        />
+        <div class="table">
+          {verbTypeOptions.map((option) => (
+            <Row {...option} onChange={toggleVerbType} />
+          ))}
+        </div>
       </Flex>
 
       <Flex column gap={4}>
         <Text>Mujarrad chapter headings</Text>
         <Segmented
-          value={option?.value}
-          options={options}
-          onChange={setOption}
+          value={settings.mujarradChapterHeadings}
+          options={mujarradChapterHeadingsOptions}
+          onChange={({ value }) =>
+            AppState.setItem('settings', {
+              ...settings,
+              mujarradChapterHeadings: value,
+            })
+          }
+        />
+      </Flex>
+
+      <Flex column gap={4}>
+        <Text>Mazeed Fihi chapter headings</Text>
+        <Segmented
+          value={settings.mazeedFihiChapterHeadings}
+          options={mazeedFihiChapterHeadingsOptions}
+          onChange={({ value }) =>
+            AppState.setItem('settings', {
+              ...settings,
+              mazeedFihiChapterHeadings: value,
+            })
+          }
         />
       </Flex>
 
       <Flex column gap={4}>
         <Text>Edit root letters</Text>
         <Segmented
-          value={option?.value}
-          options={options}
-          onChange={setOption}
+          value={settings.showRootLettersEditor}
+          options={booleanOptions}
+          onChange={({ value }) =>
+            AppState.setItem('settings', {
+              ...settings,
+              showRootLettersEditor: value,
+            })
+          }
         />
       </Flex>
 
       <Flex column gap={4}>
-        <Text>Sarf sagheer</Text>
+        <Text>Show sarf sagheer</Text>
         <Segmented
-          value={option?.value}
-          options={options}
-          onChange={setOption}
+          value={settings.showSarfSagheer}
+          options={booleanOptions}
+          onChange={({ value }) =>
+            AppState.setItem('settings', {
+              ...settings,
+              showSarfSagheer: value,
+            })
+          }
         />
       </Flex>
 
       <Flex column gap={4}>
-        <Text>Majhool</Text>
-        <Segmented
-          value={option?.value}
-          options={options}
-          onChange={setOption}
-        />
+        <Text>Tasreef options</Text>
+        <div class="table">
+          {tasreefOptions.map((option) => (
+            <Row
+              {...option}
+              onChange={(property, value) =>
+                AppState.setItem('settings', { ...settings, [property]: value })
+              }
+            />
+          ))}
+        </div>
       </Flex>
     </Flex>
   )
 }
 
 export default SettingsModal
+
+const Row = ({
+  key,
+  name,
+  value = false,
+  onChange,
+}: {
+  key: string
+  name: string
+  value?: boolean
+  onChange?: (name: string, value: boolean) => void
+}) => (
+  <div class="table-row" onClick={() => onChange?.(key, !value)}>
+    <div>{name}</div>
+    <div>
+      <input
+        type="checkbox"
+        name={name}
+        checked={value}
+        onChange={() => onChange?.(key, !value)}
+      />
+    </div>
+  </div>
+)
