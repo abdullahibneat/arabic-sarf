53a5c7559a3a67ad8cb62b4f9a31f3e19246d195
Abdullah Ibne Atiq <hello@abdu.io>
2024-04-18 20:53:43 +0100

    Store settings configuration

--- src/AppState.tsx
+++ src/AppState.tsx
@@ -4,6 +4,17 @@ export type AppStateType = {
   tasreefGroupMode: 'list' | 'by-person' | 'by-gender'
   playbackSpeed: number
   playbackLoop: boolean
+  settings: {
+    hiddenVerbTypes: string[]
+    mujarradChapterHeadings: 'arabic' | 'english' // [ن، ض، ف] / [1a, 1b, 1c]
+    mazeedFihiChapterHeadings: 'english' | 'roman' // [2, 3, 4] / [II, III, IV]
+    showRootLettersEditor: boolean
+    showSarfSagheer: boolean
+    showNasb: boolean
+    showJazm: boolean
+    showAmr: boolean
+    showMajhool: boolean
+  }
 }
 
 export type SetItemListener = (state: AppStateType) => void
@@ -12,6 +23,17 @@ const defaultState: AppStateType = {
   tasreefGroupMode: 'list',
   playbackSpeed: 1,
   playbackLoop: false,
+  settings: {
+    hiddenVerbTypes: [],
+    mujarradChapterHeadings: 'arabic',
+    mazeedFihiChapterHeadings: 'roman',
+    showRootLettersEditor: true,
+    showSarfSagheer: true,
+    showNasb: true,
+    showJazm: true,
+    showAmr: true,
+    showMajhool: true,
+  },
 }
 
 class State {
@@ -59,16 +81,7 @@ class State {
 
       if (!stateString) return
 
-      const localStorageState = JSON.parse(stateString)
-
-      for (const key of Object.keys(this.state)) {
-        if (
-          key in localStorageState &&
-          typeof localStorageState[key] === typeof this.state[key]
-        ) {
-          this.state[key] = localStorageState[key]
-        }
-      }
+      this.applyValidValues(JSON.parse(stateString), this.state)
     } catch (_) {
       // ignore errors
     }
@@ -76,6 +89,25 @@ class State {
 
   private saveToLocalStorage = () => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state))
+  }
+
+  private applyValidValues = (from: Object, to: Object) => {
+    for (const key of Object.keys(to)) {
+      if (to[key] === undefined) continue
+
+      if (Array.isArray(to[key])) {
+        if (!Array.isArray(from[key])) continue
+        from[key] = to[key]
+      } else {
+        if (typeof from[key] === 'object') {
+          if (typeof to[key] === 'object') {
+            this.applyValidValues(to[key], from[key])
+          }
+        } else {
+          from[key] = to[key]
+        }
+      }
+    }
   }
 }
 
