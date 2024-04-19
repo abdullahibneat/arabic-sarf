578d4adffc30b024246a5441e8e7a42f4bf2aabf
Abdullah Ibne Atiq <hello@abdu.io>
2024-04-19 18:45:18 +0100

    Add icon prop to MenuItem

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,8 @@
 import '../styles/MenuItem.scss'
 
+import Icon from './Icon'
 import { JSX } from 'preact'
+import MicroIcons from '../icons/micro'
 import Tag from './Tag'
 import Text from './Text'
 
@@ -8,11 +10,13 @@ export type MenuItemProps = {
   tag?: string | number
   title: string
   active?: boolean
+  icon?: keyof typeof MicroIcons
   onClick?: JSX.MouseEventHandler<HTMLDivElement>
 }
 
-const MenuItem = ({ tag, title, active, onClick }: MenuItemProps) => (
+const MenuItem = ({ tag, title, active, icon, onClick }: MenuItemProps) => (
   <div class={`menu-item ${active ? 'active' : ''}`} onClick={onClick}>
+    {icon && <Icon size="micro" name={icon} />}
     <Text type="small-medium" style={{ flex: 1 }}>
       {title}
     </Text>
