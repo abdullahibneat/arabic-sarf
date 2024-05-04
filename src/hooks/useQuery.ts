87c696f88fbd63ffeedca7f8d9b054b0d7af20c8
Abdullah Ibne Atiq <hello@abdu.io>
2024-05-04 20:10:42 +0100

    Create useNavigate and useQuery hooks

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,24 @@
+import { useCallback } from 'preact/hooks'
+import { useLocation } from 'preact-iso'
+import useNavigate from './useNavigate'
+
+const useQuery = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const get = useCallback(
+    (key: string) => (key in location.query ? location.query[key] : undefined),
+    [location.query],
+  )
+
+  const set = useCallback(
+    (key: string, value: string) => {
+      navigate(location.path, { ...location.query, [key]: String(value) }, true)
+    },
+    [location.query, navigate],
+  )
+
+  return { query: location.query, get, set }
+}
+
+export default useQuery
