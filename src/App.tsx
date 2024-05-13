f5b4cd6d8040f2deea4be0c9205f7ae24d48b35a
Abdullah Ibne Atiq <hello@abdu.io>
2024-05-13 21:11:51 +0100

    Move root letters editor and tabs from Tasreef screen to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import {
   AudioPlayerContextType,
 } from './contexts/AudioPlayerContext'
 import { ModalContext, ModalProps } from './contexts/ModalContext'
-import { useCallback, useState } from 'preact/hooks'
+import { useCallback, useMemo, useState } from 'preact/hooks'
 
 import AudioPlayer from './components/AudioPlayer'
+import { ChapterStateContext } from './contexts/ChapterStateContext'
 import IconButton from './components/IconButton'
 import { Outlet } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
+import TasreefNavigationHeader from './components/TasreefNavigationHeader'
 import Text from './components/Text'
+import replaceRoots from './helpers/replaceRoots'
+import useRouteChapter from './hooks/useRouteChapter'
 
 const App = () => {
   const [modal, setModal] = useState<ModalProps | null>(null)
@@ -19,48 +23,74 @@ const App = () => {
     close: () => null,
     setSrc: async () => {},
   })
+  const [customRootLetters, setCustomRootLetters] = useState<{
+    ف?: string
+    ع?: string
+    ل?: string
+  } | null>(null)
+
+  const baseChapter = useRouteChapter()
+
+  const chapter = useMemo(() => {
+    if (baseChapter) return replaceRoots(baseChapter, customRootLetters)
+    return null
+  }, [baseChapter, customRootLetters])
 
   const closeModal = useCallback(() => setModal(null), [])
 
   return (
     <ModalContext.Provider value={{ open: setModal, close: closeModal }}>
       <AudioPlayerContext.Provider value={audioPlayer}>
-        <div class="screen-wrapper">
-          <main>
-            <div class="screen-content">
-              <Outlet />
-            </div>
-
-            <AudioPlayer setAudioPlayer={setAudioPlayer} />
-          </main>
-
-          <Sidebar />
-        </div>
-
-        <div class="global-modal-overlay" onClick={closeModal}>
-          {modal && (
-            <div
-              class="global-modal"
-              style={{
-                width: modal.width,
-                maxWidth: modal.maxWidth,
-                height: modal.height,
-                maxHeight: modal.maxHeight,
-              }}
-              onClick={(e) => e.stopPropagation()}
-            >
-              <div class="global-modal-header">
-                <Text style={{ flex: 1 }} type="bold">
-                  {modal.title}
-                </Text>
-                <div class="global-modal-header-close">
-                  <IconButton size="micro" name="close" onClick={closeModal} />
-                </div>
+        <ChapterStateContext.Provider
+          value={{
+            baseChapter,
+            chapter,
+            customRootLetters,
+            setCustomRootLetters,
+          }}
+        >
+          <div class="screen-wrapper">
+            <main>
+              <div class="screen-content">
+                <TasreefNavigationHeader />
+                <Outlet />
               </div>
-              <div class="global-modal-content">{modal.children}</div>
-            </div>
-          )}
-        </div>
+
+              <AudioPlayer setAudioPlayer={setAudioPlayer} />
+            </main>
+
+            <Sidebar />
+          </div>
+
+          <div class="global-modal-overlay" onClick={closeModal}>
+            {modal && (
+              <div
+                class="global-modal"
+                style={{
+                  width: modal.width,
+                  maxWidth: modal.maxWidth,
+                  height: modal.height,
+                  maxHeight: modal.maxHeight,
+                }}
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div class="global-modal-header">
+                  <Text style={{ flex: 1 }} type="bold">
+                    {modal.title}
+                  </Text>
+                  <div class="global-modal-header-close">
+                    <IconButton
+                      size="micro"
+                      name="close"
+                      onClick={closeModal}
+                    />
+                  </div>
+                </div>
+                <div class="global-modal-content">{modal.children}</div>
+              </div>
+            )}
+          </div>
+        </ChapterStateContext.Provider>
       </AudioPlayerContext.Provider>
     </ModalContext.Provider>
   )
--- src/pages/TasreefScreen.tsx
+++ src/pages/TasreefScreen.tsx
@@ -1,98 +1,30 @@
 import '../styles/TasreefScreen.scss'
 
-import RootLettersEditor, { RootLetters } from '../components/RootLettersEditor'
-import { useMemo, useState } from 'preact/hooks'
-import { useParams, useSearchParams } from 'react-router-dom'
-
 import Flex from '../components/Flex'
-import IconButton from '../components/IconButton'
 import SarfSagheer from '../components/SarfSagheer'
-import Segmented from '../components/Segmented'
-import Tabs from '../components/Tabs'
 import Tasreef from '../components/Tasreef'
-import Text from '../components/Text'
 import { VerbConjugations } from '../../data/types'
 import generateEnglishTasreefs from '../helpers/generateEnglishTasreefs'
-import isMujarrad from '../helpers/isMujarrad'
-import replaceRoots from '../helpers/replaceRoots'
 import useAppState from '../hooks/useAppState'
-import verbTypes from '../../data'
+import useChapterStateContext from '../hooks/useChapterState'
+import { useMemo } from 'preact/hooks'
+import { useSearchParams } from 'react-router-dom'
 
 const TasreefScreen = () => {
-  const [rootLetters, setRootLetters] = useState<RootLetters>({
-    ف: 'ف',
-    ع: 'ع',
-    ل: 'ل',
-  })
+  const { chapter } = useChapterStateContext()
 
-  const [rootLettersEditorCollapsed, setRootLettersEditorCollapsed] =
-    useState(true)
-
-  const [searchParams, setSearchParams] = useSearchParams()
-  const params = useParams()
+  const [searchParams] = useSearchParams()
 
   const activeTab = searchParams.get('activeTab') || 'معروف'
   const verbCase = searchParams.get('verbCase') || 'مرفوع'
 
   const { settings, showEnglish } = useAppState()
 
-  const tabs = useMemo(() => {
-    const $tabs = ['معروف']
-
-    if (settings.showMajhool) $tabs.push('مجهول')
-    if (settings.showSarfSagheer) $tabs.push('صرف صغير')
-
-    return $tabs.reverse()
-  }, [settings.showMajhool, settings.showSarfSagheer])
-
-  const verbCases = useMemo(() => {
-    const $verbCases = ['مرفوع']
-
-    if (settings.showNasb) $verbCases.push('منصوب')
-    if (settings.showJazm) $verbCases.push('مجزوم')
-
-    return $verbCases.reverse()
-  }, [settings.showNasb, settings.showJazm])
-
-  const baseChapter = useMemo(() => {
-    const type = verbTypes[params.type || '']
-
-    if (!type) return null
-
-    const form = type[params.form || '']
-
-    if (!form) return null
-
-    const chapter = isMujarrad(form) ? form[params.chapter || ''] : form
-
-    if (chapter) {
-      setRootLetters({
-        ف: chapter.root_letters[0].arabic[0],
-        ع: chapter.root_letters[0].arabic[1],
-        ل: chapter.root_letters[0].arabic[2],
-      })
-
-      return chapter
-    } else {
-      return null
-    }
-  }, [params.type, params.form, params.chapter])
-
   const english = useMemo(() => {
-    if (!baseChapter) return null
+    if (!chapter) return null
     if (!showEnglish) return null
-    return generateEnglishTasreefs(baseChapter.root_letters[0].english)
-  }, [baseChapter, showEnglish])
-
-  const chapter = useMemo(() => {
-    const $chapter = baseChapter ? replaceRoots(baseChapter, rootLetters) : null
-
-    if ($chapter) {
-      document.title = $chapter.title
-    }
-
-    return $chapter
-  }, [rootLetters])
+    return generateEnglishTasreefs(chapter.root_letters[0].english)
+  }, [chapter, showEnglish])
 
   const madi = useMemo(() => {
     if (!chapter) return null
@@ -138,14 +70,14 @@ const TasreefScreen = () => {
   }, [settings.showAmr, activeTab, verbCase, chapter, english])
 
   const audioPath = useMemo(() => {
-    let path = `/recordings/${baseChapter?.type}/${baseChapter?.form}`
+    let path = `/recordings/${chapter?.type}/${chapter?.form}`
 
-    if (baseChapter?.form === 1) {
-      path += `/${baseChapter.باب}`
+    if (chapter?.form === 1) {
+      path += `/${chapter.باب}`
     }
 
     return path
-  }, [baseChapter])
+  }, [chapter])
 
   const showVerbTasreefs = useMemo(
     () => activeTab === 'معروف' || activeTab === 'مجهول',
@@ -156,79 +88,10 @@ const TasreefScreen = () => {
 
   return (
     <Flex flex={1} column gap={16} paddingBottom={32}>
-      <Flex column gap={16} paddingTop={16} backgroundColor="var(--white)">
-        <Flex column alignItems="center">
-          <div class="title">
-            <Text type="bold">{chapter.title}</Text>
-
-            {settings.showRootLettersEditor && (
-              <div class="collapse-icon">
-                <IconButton
-                  size="micro"
-                  name="chevron-down"
-                  onClick={() =>
-                    setRootLettersEditorCollapsed(!rootLettersEditorCollapsed)
-                  }
-                />
-              </div>
-            )}
-          </div>
-
-          {settings.showRootLettersEditor && (
-            <div
-              class={`root-letters-editor-revealer ${
-                rootLettersEditorCollapsed ? 'collapsed' : ''
-              }`}
-            >
-              <div class="root-letters-editor-wrapper">
-                <RootLettersEditor
-                  rootLetters={{
-                    ف: chapter.root_letters[0].arabic[0],
-                    ع: chapter.root_letters[0].arabic[1],
-                    ل: chapter.root_letters[0].arabic[2],
-                  }}
-                  mithaal={params.type === 'مثال'}
-                  ajwaf={params.type === 'أجوف'}
-                  naqis={params.type === 'ناقص'}
-                  onChange={setRootLetters}
-                />
-              </div>
-            </div>
-          )}
-        </Flex>
-
-        {tabs.length > 1 && (
-          <Tabs
-            tabs={tabs}
-            activeTab={activeTab}
-            onTabClick={(activeTab) => {
-              searchParams.set('activeTab', activeTab)
-              setSearchParams(searchParams)
-            }}
-          />
-        )}
-      </Flex>
-
       <Flex column gap={16} justifyContent="center">
         {activeTab === 'صرف صغير' && (
           <Flex column padding="0 1rem" alignItems="center">
             <SarfSagheer chapter={chapter} />
-          </Flex>
-        )}
-
-        {showVerbTasreefs && verbCases.length > 1 && (
-          <Flex alignSelf="center">
-            <Segmented
-              value={verbCase}
-              options={verbCases.map((verbCase) => ({
-                label: verbCase,
-                value: verbCase,
-              }))}
-              onChange={({ value: verbCase }) => {
-                searchParams.set('verbCase', verbCase)
-                setSearchParams(searchParams)
-              }}
-            />
           </Flex>
         )}
 
