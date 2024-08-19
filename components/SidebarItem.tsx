03fccb1cecc943b2be9627d0500231605ba5bb05
Abdullah Ibne Atiq <hello@abdu.io>
2024-08-19 19:51:08 +0100

    Improve sidebar performance

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,24 +1,25 @@
 'use client'
 
+import React, { useMemo } from 'react'
+
 import Link from 'next/link'
-import React from 'react'
 import cx from 'classix'
-import { usePathname } from 'next/navigation'
 
 type SidebarItemProps = {
   href: string
+  pathname?: string
   children: React.ReactNode
 }
 
-const SidebarItem = ({ href, children }: SidebarItemProps) => {
-  const pathname = usePathname()
-  const isActive = decodeURI(pathname) === href
+const SidebarItem = ({ href, pathname = '', children }: SidebarItemProps) => {
+  const isActive = useMemo(() => decodeURI(pathname) === href, [pathname, href])
 
   return (
     <Link
       href={href}
+      draggable={false}
       className={cx(
-        'flex h-8 items-center rounded-md border border-transparent px-2 font-medium',
+        'flex h-8 select-none items-center rounded-md border border-transparent px-2 font-medium',
         isActive && 'border-zinc-300 bg-white',
         !isActive && 'hover:border-zinc-300 hover:bg-zinc-100',
       )}
