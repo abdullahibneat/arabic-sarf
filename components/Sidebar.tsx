da8277f452d90bbbe5ba6d00bdb233f5114789f9
Abdullah Ibne Atiq <hello@abdu.io>
2024-08-24 19:30:34 +0100

    Improvements to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,19 +8,18 @@ const Sidebar = () => {
 
   return (
     <aside
-      dir="ltr"
-      className="flex w-0 flex-col md:w-[280px] [&.open]:w-[280px]"
+      className="flex w-0 shrink-0 flex-col overflow-auto md:w-[280px] [&.open]:w-[280px]"
       style={{ transition: 'width 250ms, padding 250ms' }}
     >
-      <div className="flex w-[280px] flex-col gap-2 overflow-auto p-4">
-        <Link href="/" className="flex select-none gap-2" dir="rtl">
+      <div dir="rtl" className="flex w-[280px] flex-col gap-2 p-4">
+        <Link href="/" className="flex cursor-pointer select-none gap-2">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-zinc-950 text-xs font-semibold leading-none text-white">
             ص
           </div>
           <span className="flex-grow text-xl font-bold">صرف</span>
         </Link>
 
-        <ul className="flex flex-col gap-2" dir="rtl">
+        <ul className="flex flex-col gap-2">
           {Array.from(verbTypes.entries()).map(([type, value]) => (
             <li key={type} className="flex flex-col gap-2">
               <SidebarItem href={`/${type}`} pathname={pathname}>
