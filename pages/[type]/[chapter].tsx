dee006f40848ee45473ba3232a37dfd89e0282c3
Abdullah Ibne Atiq <hello@abdu.io>
2024-08-25 16:55:07 +0100

    Initial wiring up of data to pages

--- pages/[type]/[chapter].tsx
+++ pages/[type]/[chapter].tsx
@@ -1,12 +1,74 @@
+import { VerbSighaSecondPerson, VerbTasreef } from '@/data/types'
+
+import Tasreef from '@/components/Tasreef'
+import { useMemo } from 'react'
 import { useRouter } from 'next/router'
+import verbTypes from '@/data'
 
 const Chapter = () => {
   const { type, chapter } = useRouter().query
+
+  const tasreefs = useMemo(() => {
+    if (!type) return null
+
+    const $tasreefs: Array<{
+      name: string
+      tasreef:
+        | VerbTasreef
+        | {
+            '2nd': VerbSighaSecondPerson
+          }
+    }> = []
+
+    const verbType = verbTypes.get(String(type))
+
+    if (!verbType) return null
+
+    const verbChapter = verbType.get(String(chapter))
+
+    if (!verbChapter) return null
+
+    $tasreefs.push({
+      name: 'ماضي',
+      tasreef: verbChapter.فعل.ماضي.معروف.مرفوع,
+    })
+
+    $tasreefs.push({
+      name: 'مضارع',
+      tasreef: verbChapter.فعل.مضارع.معروف.مرفوع,
+    })
+
+    const amr = verbChapter.فعل.أمر.معروف.مجزوم
+
+    if (amr) {
+      $tasreefs.push({
+        name: 'أمر',
+        tasreef: amr,
+      })
+    }
+
+    return $tasreefs
+  }, [type, chapter])
+
+  if (!tasreefs) return <div className="p-4">not found</div>
+
   return (
     <div className="p-4">
-      <h1>
-        {type} - {chapter}
-      </h1>
+      <div>
+        <h2>
+          {type} - {chapter}
+        </h2>
+        <div className="flex gap-1">
+          {tasreefs.map(({ name, tasreef }, tasreefIndex) => (
+            <Tasreef
+              key={tasreefIndex}
+              name={name}
+              tasreef={tasreef}
+              mode="list"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- pages/[type]/index.tsx
+++ pages/[type]/index.tsx
@@ -1,10 +1,50 @@
+import Tasreef from '@/components/Tasreef'
+import { VerbTasreef } from '@/data/types'
+import { useMemo } from 'react'
 import { useRouter } from 'next/router'
+import verbTypes from '@/data'
 
 const Type = () => {
   const { type } = useRouter().query
+
+  const tasreefs = useMemo(() => {
+    if (!type) return null
+
+    const $tasreefs: Array<{ name: string; tasreef: VerbTasreef }> = []
+
+    const verbType = verbTypes.get(String(type))
+
+    if (!verbType) return null
+
+    for (const chapterName of Array.from(verbType.keys())) {
+      const chapter = verbType.get(chapterName)
+      if (!chapter) continue
+      $tasreefs.push({
+        name: chapterName,
+        tasreef: chapter.فعل.ماضي.معروف.مرفوع,
+      })
+    }
+
+    return $tasreefs
+  }, [type])
+
+  if (!tasreefs) return <div className="p-4">not found</div>
+
   return (
     <div className="p-4">
-      <h1>{type}</h1>
+      <div>
+        <h2>{type}</h2>
+        <div className="flex gap-1">
+          {tasreefs.map(({ name, tasreef }, tasreefIndex) => (
+            <Tasreef
+              key={tasreefIndex}
+              name={name}
+              tasreef={tasreef}
+              mode="list"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
