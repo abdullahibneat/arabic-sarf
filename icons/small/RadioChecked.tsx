44311a75c2c4bdcdac4d257f8866ebfa5db76551
Abdullah Ibne Atiq <hello@abdu.io>
2024-10-05 21:15:06 +0100

    Add more icons

---
+++ icons/small/RadioChecked.tsx
@@ -0,0 +1,14 @@
+const RadioChecked = () => (
+  <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="6" cy="6" r="4.5" stroke="currentColor" />
+    <circle cx="6" cy="6" r="2" fill="currentColor" />
+  </svg>
+)
+
+export default RadioChecked
--- icons/small/index.ts
+++ icons/small/index.ts
@@ -1,11 +1,15 @@
 import Chevron from './Chevron'
 import Close from './Close'
+import Radio from './Radio'
+import RadioChecked from './RadioChecked'
 import Settings from './Settings'
 
 const smallIcons = {
   chevron: Chevron,
   close: Close,
   settings: Settings,
+  radio: Radio,
+  'radio-checked': RadioChecked,
 }
 
 export type SmallIconName = keyof typeof smallIcons
