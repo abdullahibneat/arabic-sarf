ce68df3036bca653859569c0ea69885c1433a3d1
Abdullah Ibne Atiq <hello@abdu.io>
2024-10-15 21:05:33 +0100

    Import more icons

--- icons/small/index.ts
+++ icons/small/index.ts
@@ -1,5 +1,8 @@
 import Chevron from './Chevron'
 import Close from './Close'
+import Loop from './Loop'
+import Pause from './Pause'
+import Play from './Play'
 import Radio from './Radio'
 import RadioChecked from './RadioChecked'
 import Settings from './Settings'
@@ -7,6 +10,9 @@ import Settings from './Settings'
 const smallIcons = {
   chevron: Chevron,
   close: Close,
+  loop: Loop,
+  play: Play,
+  pause: Pause,
   settings: Settings,
   radio: Radio,
   'radio-checked': RadioChecked,
