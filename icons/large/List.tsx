c608e4bdbf4a20bed5cf627f08fb1b9d173e07ee
Abdullah Ibne Atiq <hello@abdu.io>
2024-10-15 20:33:21 +0100

    Make icons responsive

--- icons/large/List.tsx
+++ icons/large/List.tsx
@@ -1,14 +1,14 @@
 const List = () => (
   <svg
-    width="25"
-    height="24"
-    viewBox="0 0 25 24"
+    width="1.5rem"
+    height="1.5rem"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <rect x="4.5" y="4" width="16" height="4" rx="2" fill="currentColor" />
-    <rect x="4.5" y="10" width="16" height="4" rx="2" fill="currentColor" />
-    <rect x="4.5" y="16" width="16" height="4" rx="2" fill="currentColor" />
+    <rect x="4" y="4" width="16" height="4" rx="2" fill="currentColor" />
+    <rect x="4" y="10" width="16" height="4" rx="2" fill="currentColor" />
+    <rect x="4" y="16" width="16" height="4" rx="2" fill="currentColor" />
   </svg>
 )
 
--- icons/small/Chevron.tsx
+++ icons/small/Chevron.tsx
@@ -1,7 +1,7 @@
 const Chevron = () => (
   <svg
-    width="12"
-    height="12"
+    width="0.75rem"
+    height="0.75rem"
     viewBox="0 0 12 12"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
