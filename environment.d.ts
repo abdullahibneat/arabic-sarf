c647375aa111698630adfddb75c105837c350302
Abdullah Ibne Atiq <hello@abdu.io>
2024-10-20 20:42:53 +0100

    Configure posthog

---
+++ environment.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_POSTHOG_KEY: string
+    NEXT_PUBLIC_POSTHOG_HOST: string
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,11 @@ import { useCallback, useEffect, useState } from 'react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Island from '@/components/Island'
+import { PostHogProvider } from 'posthog-js/react'
 import { SarfContext } from '@/contexts/SarfContext'
 import Sidebar from '@/components/Sidebar'
 import { createPortal } from 'react-dom'
+import posthog from 'posthog-js'
 import { useAtom } from 'jotai'
 import useHead from '@/hooks/useHead'
 import useMatchMedia from '@/hooks/useMatchMedia'
@@ -25,6 +27,18 @@ const notoSansArabic = Noto_Sans_Arabic({
 const notoSans = Noto_Sans({
   subsets: ['latin'],
 })
+
+if (typeof window !== 'undefined') {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host:
+      process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+    autocapture: false,
+  })
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [sarfType, setSarfType] = useState('صرف كبير')
@@ -84,85 +98,87 @@ const App = ({ Component, pageProps }: AppProps) => {
    */
 
   return (
-    <ModalContext.Provider
-      value={{ present: presentModal, dismiss: dismissModal }}
-    >
-      <SarfContext.Provider
-        value={{
-          verbType: !type ? null : String(type),
-          verbChapter: !chapter ? null : String(chapter),
-          verbCase,
-          sarfType,
-          passive,
-          rootLetters,
-        }}
+    <PostHogProvider client={posthog}>
+      <ModalContext.Provider
+        value={{ present: presentModal, dismiss: dismissModal }}
       >
-        <style jsx global>{`
-          html {
-            font-family: ${notoSansArabic.style.fontFamily},
-              ${notoSans.style.fontFamily}, sans-serif;
-            font-size: ${fontSize}px;
-          }
-        `}</style>
+        <SarfContext.Provider
+          value={{
+            verbType: !type ? null : String(type),
+            verbChapter: !chapter ? null : String(chapter),
+            verbCase,
+            sarfType,
+            passive,
+            rootLetters,
+          }}
+        >
+          <style jsx global>{`
+            html {
+              font-family: ${notoSansArabic.style.fontFamily},
+                ${notoSans.style.fontFamily}, sans-serif;
+              font-size: ${fontSize}px;
+            }
+          `}</style>
 
-        <Head>
-          <title>{head.title}</title>
-          <meta name="description" content={head.description} />
+          <Head>
+            <title>{head.title}</title>
+            <meta name="description" content={head.description} />
 
-          <meta property="og:title" content={head.title} />
-          <meta property="og:description" content={head.description} />
-          <meta property="og:image" content={head.image} />
+            <meta property="og:title" content={head.title} />
+            <meta property="og:description" content={head.description} />
+            <meta property="og:image" content={head.image} />
 
-          {/* Favicon */}
-          <link rel="icon" href="/favicon.ico" sizes="48x48" />
-          <link
-            rel="icon"
-            href="/favicon.svg"
-            sizes="any"
-            type="image/svg+xml"
-          />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        </Head>
-
-        <div className="flex h-full overflow-hidden">
-          <main
-            dir="rtl"
-            className="relative z-[1] w-0 flex-grow rounded-lg bg-white p-4 dark:bg-neutral-900"
-          >
-            <div className="absolute inset-0 overflow-auto">
-              <Component {...pageProps} />
-            </div>
-
-            <Island
-              sarfType={sarfType}
-              verbCase={verbCase}
-              passive={passive}
-              rootLetters={rootLetters}
-              setSarfType={setSarfType}
-              setVerbCase={setVerbCase}
-              setPassive={setPassive}
-              setRootLetters={setRootLetters}
+            {/* Favicon */}
+            <link rel="icon" href="/favicon.ico" sizes="48x48" />
+            <link
+              rel="icon"
+              href="/favicon.svg"
+              sizes="any"
+              type="image/svg+xml"
             />
-          </main>
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          </Head>
 
-          <Sidebar />
-        </div>
-
-        {modals.map((modal) =>
-          createPortal(
-            <dialog
-              id={modal.id}
-              onCancel={() => dismissModal(modal.id)}
-              className="m-0 h-full max-h-full w-full max-w-full items-center justify-center open:flex"
+          <div className="flex h-full overflow-hidden">
+            <main
+              dir="rtl"
+              className="relative z-[1] w-0 flex-grow rounded-lg bg-white p-4 dark:bg-neutral-900"
             >
-              {modal.content}
-            </dialog>,
-            document.body,
-            modal.id,
-          ),
-        )}
-      </SarfContext.Provider>
-    </ModalContext.Provider>
+              <div className="absolute inset-0 overflow-auto">
+                <Component {...pageProps} />
+              </div>
+
+              <Island
+                sarfType={sarfType}
+                verbCase={verbCase}
+                passive={passive}
+                rootLetters={rootLetters}
+                setSarfType={setSarfType}
+                setVerbCase={setVerbCase}
+                setPassive={setPassive}
+                setRootLetters={setRootLetters}
+              />
+            </main>
+
+            <Sidebar />
+          </div>
+
+          {modals.map((modal) =>
+            createPortal(
+              <dialog
+                id={modal.id}
+                onCancel={() => dismissModal(modal.id)}
+                className="m-0 h-full max-h-full w-full max-w-full items-center justify-center open:flex"
+              >
+                {modal.content}
+              </dialog>,
+              document.body,
+              modal.id,
+            ),
+          )}
+        </SarfContext.Provider>
+      </ModalContext.Provider>
+    </PostHogProvider>
   )
 }
 
