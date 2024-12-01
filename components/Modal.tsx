8f9ef6773b79a41bf35a15aefb3d0bf866585a6d
Abdullah Ibne Atiq <hello@abdu.io>
2024-12-01 16:35:04 +0000

    Add prop to close modals by clicking outside

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,15 +1,31 @@
 import IconButton from './IconButton'
+import useOnClickOutside from '@/hooks/useOnClickOutside'
+import { useRef } from 'react'
 
 export type ModalProps = {
   title: string
   style?: React.CSSProperties
   children: React.ReactNode
+  closeOnClickOutside?: boolean
   onClose?: () => void
 }
 
-const Modal = ({ title, style, children, onClose }: ModalProps) => {
+const Modal = ({
+  title,
+  style,
+  children,
+  closeOnClickOutside = true,
+  onClose,
+}: ModalProps) => {
+  const modal = useRef<HTMLDivElement>(null)
+
+  useOnClickOutside(modal, () => {
+    if (closeOnClickOutside) onClose?.()
+  })
+
   return (
     <div
+      ref={modal}
       className="flex h-full w-full flex-col rounded-lg bg-zinc-50 p-2 text-zinc-900 dark:bg-neutral-800 dark:text-neutral-100"
       style={style}
     >
