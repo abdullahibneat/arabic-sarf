637d3db4dd7f5f8f1b61f299a92f0d5239704d22
Abdullah Ibne Atiq <hello@abdu.io>
2024-12-02 08:00:46 +0000

    Add ellipsis to page title

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,9 +1,10 @@
-const PageTitle = ({ children }: { children: string }) => {
-  return (
-    <div className="pointer-events-none sticky top-0 z-10 flex h-10 items-center justify-center">
-      <h2 className="pointer-events-auto">{children}</h2>
-    </div>
-  )
-}
+const PageTitle = ({ children }: { children: string }) => (
+  /** pointer-events is used so that clicks can be passed through to the <PageHeader /> buttons */
+  <div className="pointer-events-none sticky top-0 z-10 flex h-10 items-center justify-center px-16">
+    <h2 className="pointer-events-auto overflow-hidden text-ellipsis text-nowrap">
+      {children}
+    </h2>
+  </div>
+)
 
 export default PageTitle
